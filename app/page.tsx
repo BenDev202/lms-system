40f1188abc1df87a1c8cc5e3247a3017fe315c45
armand benjamin <armandbenjamin30@gmail.com>
2025-06-23 07:29:17 +0200

    feat: integrate ThemeProvider and add ModeToggle component for theme management

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { ModeToggle } from "@/components/ModeToggle";
 
 export default function Home() {
   return (
     <div>
-      
+      <ModeToggle />
     </div>
   );
 }
