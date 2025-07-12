1d45b48eb04ef0b684b8d089b49a19993f74d690
armand benjamin <armandbenjamin30@gmail.com>
2025-07-12 21:18:54 +0200

    updating the db and intergrate with  login and remove any security issues when user is sing in

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
+import { redirect } from 'next/navigation';
+import { headers } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  "use server"
+  const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if(session) {
+        return redirect("/")
+    }
   return (
     <div>
       <ModeToggle />
+
+      {session ? <p>
+        {sessionStorage.user.name}
+      </p> : <Button>Logout</Button>}
     </div>
   );
 }
